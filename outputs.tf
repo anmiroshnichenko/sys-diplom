f142a5efcf1aa8e114af255281ab0f825b5501f1
Aleksandr Miroshnichenko <119445763+anmiroshnichenko@users.noreply.github.com>
2023-09-12 16:24:22 +0300

    Add files via upload

---
+++ outputs.tf
@@ -0,0 +1,21 @@
+
+output "maintest_vm1" {
+value = yandex_compute_instance.vm-1.boot_disk[0].disk_id
+}
+
+output "external_vm1" {
+value = yandex_compute_instance.vm-1.network_interface[0].nat_ip_address
+}
+
+
+output "external_vm2" {
+value = yandex_compute_instance.vm-2.network_interface[0].nat_ip_address
+}
+
+output "internal_vm1" {
+value = yandex_compute_instance.vm-1.network_interface[0].ip_address
+}
+
+output "internal_vm2" {
+value = yandex_compute_instance.vm-2.network_interface[0].ip_address
+}
