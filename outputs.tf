dbebaadfb46d75691c5a9e23474628928762e22f
Aleksandr Miroshnichenko <119445763+anmiroshnichenko@users.noreply.github.com>
2023-09-13 15:21:10 +0300

    Add files via upload

--- outputs.tf
+++ outputs.tf
@@ -10,7 +10,6 @@ output "external_vm1" {
 value = yandex_compute_instance.vm-1.network_interface[0].nat_ip_address
 }
 
-
 output "external_vm2" {
 value = yandex_compute_instance.vm-2.network_interface[0].nat_ip_address
 }
@@ -21,4 +20,12 @@ value = yandex_compute_instance.vm-1.network_interface[0].ip_address
 
 output "internal_vm2" {
 value = yandex_compute_instance.vm-2.network_interface[0].ip_address
+}
+
+output "target_group_ids" {
+value = yandex_lb_target_group.web-servers.id
+}
+
+output "lb_ip_address" {
+  value = yandex_lb_network_load_balancer.lb-netology.*
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "zone" {
 type = string
 default = "ru-central1-a"
 }
+
+variable "zone1" {
+type = string
+default = "ru-central1-b"
+}
+
+variable "vm_image_family" {
+type = string
+default = "lemp"
+}
