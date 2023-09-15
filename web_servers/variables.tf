19de31321d7ae70c69826b3d7bd17834e30d486b
Aleksandr Miroshnichenko <119445763+anmiroshnichenko@users.noreply.github.com>
2023-09-15 20:36:49 +0300

    Update variables.tf

--- web_servers/variables.tf
+++ web_servers/variables.tf
@@ -19,5 +19,6 @@ variable "yc_image_family" {
 }
 
 variable "image_id" {
-  default = "fd8d8etig5vu92nh75bm" # it's ubuntu-2004-lts
+  #default = "fd8d8etig5vu92nh75bm" # it's ubuntu-2004-lts
+  default = "fd8vtq76jue50g6b6tm7"  # it's debian-11
 }
